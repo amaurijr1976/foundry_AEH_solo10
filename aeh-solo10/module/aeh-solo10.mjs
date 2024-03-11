54925f22bd051e836b16809e32659e901881adbc
John Doe <johndoe@example.com>
2024-03-11 08:54:55 -0300

    versão inicial

---
+++ aeh-solo10/module/aeh-solo10.mjs
@@ -0,0 +1,143 @@
+// Import document classes.
+import { AEHSolo10Actor } from './documents/actor.mjs';
+import { AEHSolo10Item } from './documents/item.mjs';
+// Import sheet classes.
+import { AEHSolo10ActorSheet } from './sheets/actor-sheet.mjs';
+import { AEHSolo10ItemSheet } from './sheets/item-sheet.mjs';
+// Import helper/utility classes and constants.
+import { preloadHandlebarsTemplates } from './helpers/templates.mjs';
+import { AEHSOLO10 } from './helpers/config.mjs';
+
+/* -------------------------------------------- */
+/*  Init Hook                                   */
+/* -------------------------------------------- */
+
+Hooks.once('init', function () {
+  // Add utility classes to the global game object so that they're more easily
+  // accessible in global contexts.
+  game.aehsolo10 = {
+    AEHSolo10Actor,
+    AEHSolo10Item,
+    rollItemMacro,
+  };
+
+  // Add custom constants for configuration.
+  CONFIG.AEHSOLO10 = AEHSOLO10;
+
+  /**
+   * Set an initiative formula for the system
+   * @type {String}
+   */
+  CONFIG.Combat.initiative = {
+    formula: '1d20 + @abilities.dex.mod',
+    decimals: 2,
+  };
+
+  // Define custom Document classes
+  CONFIG.Actor.documentClass = AEHSolo10Actor;
+  CONFIG.Item.documentClass = AEHSolo10Item;
+
+  // Active Effects are never copied to the Actor,
+  // but will still apply to the Actor from within the Item
+  // if the transfer property on the Active Effect is true.
+  CONFIG.ActiveEffect.legacyTransferral = false;
+
+  // Register sheet application classes
+  Actors.unregisterSheet('core', ActorSheet);
+  Actors.registerSheet('aeh-solo10', AEHSolo10ActorSheet, {
+    makeDefault: true,
+    label: 'AEHSOLO10.SheetLabels.Actor',
+  });
+  Items.unregisterSheet('core', ItemSheet);
+  Items.registerSheet('aeh-solo10', AEHSolo10ItemSheet, {
+    makeDefault: true,
+    label: 'AEHSOLO10.SheetLabels.Item',
+  });
+
+  // Preload Handlebars templates.
+  return preloadHandlebarsTemplates();
+});
+
+/* -------------------------------------------- */
+/*  Handlebars Helpers                          */
+/* -------------------------------------------- */
+
+// If you need to add Handlebars helpers, here is a useful example:
+Handlebars.registerHelper('toLowerCase', function (str) {
+  return str.toLowerCase();
+});
+
+/* -------------------------------------------- */
+/*  Ready Hook                                  */
+/* -------------------------------------------- */
+
+Hooks.once('ready', function () {
+  // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
+  Hooks.on('hotbarDrop', (bar, data, slot) => createItemMacro(data, slot));
+});
+
+/* -------------------------------------------- */
+/*  Hotbar Macros                               */
+/* -------------------------------------------- */
+
+/**
+ * Create a Macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {Object} data     The dropped data
+ * @param {number} slot     The hotbar slot to use
+ * @returns {Promise}
+ */
+async function createItemMacro(data, slot) {
+  // First, determine if this is a valid owned item.
+  if (data.type !== 'Item') return;
+  if (!data.uuid.includes('Actor.') && !data.uuid.includes('Token.')) {
+    return ui.notifications.warn(
+      'You can only create macro buttons for owned Items'
+    );
+  }
+  // If it is, retrieve it based on the uuid.
+  const item = await Item.fromDropData(data);
+
+  // Create the macro command using the uuid.
+  const command = `game.aehsolo10.rollItemMacro("${data.uuid}");`;
+  let macro = game.macros.find(
+    (m) => m.name === item.name && m.command === command
+  );
+  if (!macro) {
+    macro = await Macro.create({
+      name: item.name,
+      type: 'script',
+      img: item.img,
+      command: command,
+      flags: { 'aeh-solo10.itemMacro': true },
+    });
+  }
+  game.user.assignHotbarMacro(macro, slot);
+  return false;
+}
+
+/**
+ * Create a Macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {string} itemUuid
+ */
+function rollItemMacro(itemUuid) {
+  // Reconstruct the drop data so that we can load the item.
+  const dropData = {
+    type: 'Item',
+    uuid: itemUuid,
+  };
+  // Load the item from the uuid.
+  Item.fromDropData(dropData).then((item) => {
+    // Determine if the item loaded and if it's an owned item.
+    if (!item || !item.parent) {
+      const itemName = item?.name ?? itemUuid;
+      return ui.notifications.warn(
+        `Could not find item ${itemName}. You may need to delete and recreate this macro.`
+      );
+    }
+
+    // Trigger the item roll
+    item.roll();
+  });
+}
---
+++ aeh-solo10/module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const AEHSOLO10 = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+AEHSOLO10.abilities = {
+  str: 'AEHSOLO10.Ability.Str.long',
+  dex: 'AEHSOLO10.Ability.Dex.long',
+  con: 'AEHSOLO10.Ability.Con.long',
+  int: 'AEHSOLO10.Ability.Int.long',
+  wis: 'AEHSOLO10.Ability.Wis.long',
+  cha: 'AEHSOLO10.Ability.Cha.long',
+};
+
+AEHSOLO10.abilityAbbreviations = {
+  str: 'AEHSOLO10.Ability.Str.abbr',
+  dex: 'AEHSOLO10.Ability.Dex.abbr',
+  con: 'AEHSOLO10.Ability.Con.abbr',
+  int: 'AEHSOLO10.Ability.Int.abbr',
+  wis: 'AEHSOLO10.Ability.Wis.abbr',
+  cha: 'AEHSOLO10.Ability.Cha.abbr',
+};
